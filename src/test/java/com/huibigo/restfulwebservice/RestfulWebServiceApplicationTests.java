af6c00a6b3727a774a3164a43190c6b3cec06064
KimSungEun226 <tjdt11@naver.com>
2023-09-10 22:02:05 +0900

    Spring Boot API 사용

--- src/test/java/com/huibigo/restfulwebservice/RestfulWebServiceApplicationTests.java
+++ src/test/java/com/huibigo/restfulwebservice/RestfulWebServiceApplicationTests.java
@@ -1,13 +1,10 @@
 package com.huibigo.restfulwebservice;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RestfulWebServiceApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+
 
 }
