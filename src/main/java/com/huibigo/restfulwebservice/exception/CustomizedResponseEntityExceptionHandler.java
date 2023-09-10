ac32c1efe7e2c6062703de0fa79eef177d6bf1aa
KimSungEun226 <tjdt11@naver.com>
2023-09-10 14:33:29 +0900

    다국어, 유효성처리, filtering, API 버전관리

--- src/main/java/com/huibigo/restfulwebservice/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/huibigo/restfulwebservice/exception/CustomizedResponseEntityExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.huibigo.restfulwebservice.exception;
 
 import com.huibigo.restfulwebservice.user.UserNotFoundException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,6 +31,16 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
                 new ExceptionReponse(new Date(), ex.getMessage(), request.getDescription(false));
 
         return new ResponseEntity(exceptionReponse, HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatus status,
+                                                                  WebRequest request) {
+        ExceptionReponse exceptionReponse = new ExceptionReponse(new Date(),
+                "Validation Failed", ex.getBindingResult().toString());
+        return new ResponseEntity(exceptionReponse, HttpStatus.BAD_REQUEST);
     }
 }
 
